bc5e9e7d8b9693b8bcba2cb45f0fffd789f84559
James Macbook <jamesfhoover@gmail.com>
2015-06-04 00:53:45 -0700

    organized files

--- slick/slick-settings.js
+++ slick/slick-settings.js
@@ -1,13 +1,18 @@
-$(document).ready(function(){
-      $('.slick_slider').slick({
-      		dots: true,
-          infinite: true,
-          speed: 500,
-          arrows: true,
+/* 
+** slick-settings.js
+** Custom slick setting for www.206tutoring.com
+** http://kenwheeler.github.io/slick/
+*/
 
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-      });
-    });
+$(document).ready(function(){
+  $('.slick-slider').slick({
+      infinite: true,
+      speed: 1000,
+      arrows: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 7000,
+      pauseOnHover: false
+  });
+}); 
